04a7a6a932d4b1f651b02a53c4ed9a8d24484e71
joelazwar <joel_azwar@yahoo.com>
2022-01-14 20:23:02 -0500

    datepicker test

--- pages/api-test.js
+++ pages/api-test.js
@@ -1,12 +1,14 @@
 import useSWR from "swr";
 import Layout from "../components/layout";
-import Link from "next/link";
 import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
 import Head from "next/head";
 import Image from "next/image";
+import DatePicker from "react-datepicker"
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const myLoader = ({ src, date, startDate }) => ``;
 
 function Content() {
   const { data, error } = useSWR(
@@ -17,43 +19,58 @@ function Content() {
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
-  console.log(data);
-  // render data
   return (
-    <ul>
-      {data.map(({ copyright, date, explanation, url, title }, index) => (
-        <li className={utilStyles.listItem} key={index}>
-          <Image
-            src={url}
-            alt={copyright}
-            width="100%"
-            height="100%"
-            layout="responsive"
-            objectFit="contain"
-          />
-          <Head>
-            <title>{title}</title>
-          </Head>
-          <article>
-            <h1 className={utilStyles.headingXl}>{data.title}</h1>
-            <div className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </div>
-          </article>
-          <Link href="/">
-            <a>{title}</a>
-          </Link>
-          <br />
-          <small className={utilStyles.lightText}></small>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {data.map(
+        ({ copyright, date, explanation, hdurl, url, title }, index) => (
+          <li className={utilStyles.listItem} key={index}>
+            <a href={hdurl}>
+              <Image
+                loader={myLoader}
+                src={url}
+                alt={copyright}
+                width="100%"
+                height="100%"
+                layout="responsive"
+                objectFit="contain"
+              />
+            </a>
+            <article>
+              <h1 className={utilStyles.headingXl}>{data.title}</h1>
+              <div className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </div>
+            </article>
+            <a href={hdurl}>{title}</a>
+            <br />
+            <small className={utilStyles.lightText}></small>
+          </li>
+        )
+      )}
+    </div>
   );
 }
 
 export default function Page() {
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  
   return (
     <Layout>
+      <Head>
+        <title>NASA APOD</title>
+      </Head>
+      <div>
+        <DatePicker
+        selected={date}
+        onChange={(date) => setDate(date)}
+        />
+        <DatePicker
+        selected={startDate}
+        onChange={(startDate) => setStartDate(startDate)}
+        />
+      </div>
       <div className="container">
         <Content />
       </div>
